f356977d23435a2f0367d9f9d1ce3ab1f02003d7
dnns <dennis.ah@gmail.com>
2023-06-12 14:06:50 -0500

    first commit

---
+++ src/main/java/com/nttdata/configurator/ConfiguratorApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.configurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguratorApplication.class, args);
+	}
+
+}
